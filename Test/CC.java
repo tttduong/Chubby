46891a57d72d6e04a030436e34f2c2e766d546f6
tttduong <tttduong23@gmail.com>
2023-10-21 15:00:00 +0700

    Create CC.java

---
+++ Test/CC.java
@@ -0,0 +1,15 @@
+
+package cc;
+
+import java.io.FileNotFoundException;
+
+public class CC {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        
+        Question q = new Question();
+        q.setVisible(true);
+        
+    }
+    
+}
